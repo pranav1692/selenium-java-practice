6f79c3eb421021da24ab2e418a11b40a6896e9bb
Pranav Patil <pranavpatil2353@gmail.com>
2026-04-13 22:35:17 +0530

    Added Explicit wait inside LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -5,14 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
+
     public LoginPage(WebDriver driver){
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         PageFactory.initElements(driver, this);
     }
+
 
     @FindBy(id = "UserName")
     private WebElement txtUserName;
@@ -24,9 +33,14 @@ public class LoginPage {
     private WebElement btnLogin;
 
     public HomePage performLogin(String username, String password){
-        UiElementExtension.performEnterText(txtUserName, username);
-        UiElementExtension.performEnterText(txtPassword, password);
+        //UiElementExtension.performEnterText(txtUserName, username);
+        //UiElementExtension.performEnterText(txtPassword, password);
+        wait.until(ExpectedConditions.visibilityOf(txtUserName)).sendKeys(username);
+        wait.until(ExpectedConditions.visibilityOf(txtPassword)).sendKeys(password);
         UiElementExtension.performClick(btnLogin);
+
+
+
         return  new HomePage(driver);
     }
 }
